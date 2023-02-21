1f11b67f835a962a215ead1ad84d3a852364055e
lkdoe <leonhard.doerflinger@gmail.com>
2023-02-21 18:20:26 +0100

    conversion from primitive synth to keyboard synth

--- FinFETs Synth/keyboard_synth.py
+++ FinFETs Synth/keyboard_synth.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import numpy as np
+import time
 
 pg.init()
 pg.mixer.init()
@@ -8,6 +9,10 @@ sampling_rate = 44100
 frequency = 440
 duration = 1.5
 frames = int(duration * sampling_rate)
-arr = np.cos(2 * np.pi * frequency)
+arr = np.cos(2 * np.pi * frequency * np.linspace(0, duration, frames))
+print(arr)
+sound = np.asarray([32767*arr, 32767*arr]).T.astype(np.int16)
+sound = pg.sndarray.make_sound(sound.copy())
 
-sound = np.asarray([])
+sound.play()
+time.sleep(5)
